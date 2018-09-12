8e341af9949a23cd9af285f883454ee9b99b09a3
Snyp54 <38736485+Snyp54@users.noreply.github.com>
2018-09-12 16:33:37 +0200

    Delete dbconnect.php

--- dbconnect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$Username = "root";  // enter your username for mysql
-$Password = "password";  // enter your password for mysql
-$Hostname = "localhost";      // this is usually "localhost" unless your database resides on a different server
-$Database = "energie"; //database name
-
-$dbh = mysql_connect($Hostname , $Username, $Password) or die (mysql_error());;
-@mysql_select_db($Database) or die (mysql_error());
-?>
