7d8a5874cdaf693726453ffd42dcc549fb042ba9
Snyp54 <38736485+Snyp54@users.noreply.github.com>
2018-12-16 11:19:03 +0100

    Update main.ino
    add bme280,HTUD21

--- main.ino
+++ main.ino
@@ -1,33 +1,92 @@
-#include <Wire.h>
-#include "SparkFunHTU21D.h"
+/***************************************************************************
+  This is a library for the BME280 humidity, temperature & pressure sensor
 
-//Create an instance of the object
+  Designed specifically to work with the Adafruit BME280 Breakout
+  ----> http://www.adafruit.com/products/2650
+
+  These sensors use I2C or SPI to communicate, 2 or 4 pins are required
+  to interface. The device's I2C address is either 0x76 or 0x77.
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit andopen-source hardware by purchasing products
+  from Adafruit!
+
+  Written by Limor Fried & Kevin Townsend for Adafruit Industries.
+  BSD license, all text above must be included in any redistribution
+ ***************************************************************************/
+/*Ici on défini*/
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+#include "SparkFunHTU21D.h"
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_BME280 bme; // I2C
 HTU21D myHumidity;
 
-unsigned long startMillis;  //some global variables available anywhere in the program
-unsigned long currentMillis;
-const unsigned long period = 1 * 60 * 1000UL ;  //valeur en millisecondes pour l'envoie des données
+/*Ici on défini le l'intervalle de temp pour la transmission des donées*/
+unsigned long period = 1000UL * 30;
+unsigned long time_now = 0;
+
+float minTemp; // keep track of minimum recorded temp
+float maxTemp; // keep track of maximum recorded temp
+
+void setup() {
+  Serial.begin(9600);
+
+  if (! bme.begin(&Wire)) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1);
+  }
+  myHumidity.begin();
+  minTemp = myHumidity.readTemperature();
+  maxTemp = myHumidity.readTemperature();
+
+}
+void loop() {
+  // Only needed in forced mode! In normal mode, you can remove the next line.
+  bme.takeForcedMeasurement(); // has no effect in normal mode
+
+  // update min and max temp values
+  if (myHumidity.readTemperature() < minTemp) {
+    minTemp = myHumidity.readTemperature();
+  }
+  if (myHumidity.readTemperature() > maxTemp) {
+    maxTemp = myHumidity.readTemperature();
+  }
+
+  //Ici on utilise le time millis() déclaré plus haut.A chaque fois que l'on atteind le conmpteur, on execute void printValues().
+  if (millis() > time_now + period) {
+    time_now = millis();
+    printValues();
+  }
 
 
-void setup()
-{
-  Serial.begin(9600); // on initialise le port série à 9600
-  myHumidity.begin(); //on initialise le HTU21D
-  startMillis = millis();  //initial start time
 }
 
-void loop()
-{
-  float h = myHumidity.readHumidity();
-  float t = myHumidity.readTemperature();
-  
-  currentMillis = millis();
-  if (currentMillis - startMillis >=  period)
-  {
-  Serial.print(t, 1); //affiche la temperature
-  Serial.print(" \t"); 
-  Serial.print(h, 1); //affiche l'humidité
+
+void printValues() {
+  float humd = myHumidity.readHumidity();
+  float temp = myHumidity.readTemperature();
+  float tempp = bme.readTemperature();
+  float humdp = bme.readHumidity();
+  float pres = bme.readPressure() / 100.0F + 23.6;
+
+  Serial.print(temp, 1);
+  Serial.print(" \t");
+  Serial.print(minTemp, 1);
+  Serial.print(" \t");
+  Serial.print(maxTemp, 1);
+  Serial.print(" \t");
+  Serial.print(humd, 1);
+  Serial.print(" \t");
+  Serial.print(tempp, 1);
+  Serial.print(" \t");
+  Serial.print(humdp, 1);
+  Serial.print(" \t");
+  Serial.print(pres, 1);
+  Serial.print(" \t");
   Serial.println();
-  startMillis = currentMillis;
-  }
+
 }
